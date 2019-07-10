45c8bc60793de71b1cb63a9039a823cf199275ef
alecc2m2 <45518779+alecc2m2@users.noreply.github.com>
2019-07-10 17:31:00 +0900

    alexs-portfolio

---
+++ alexs-portfolio/script.js
@@ -0,0 +1,28 @@
+$(function(){
+
+// ヘッダーリンク
+  $('.header-nav li a').click(function(){
+      var id = $(this).attr('href');
+      var position = $(id).offset().top;
+      $('html,body').animate({'scrollTop':position},500);
+		});
+
+// トップへ戻るボタンの出現
+  var $pageTop = $(".pagetop")
+  $pageTop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 400) {
+          $pageTop.fadeIn();
+      } else {
+          $pageTop.fadeOut();
+      }
+  });
+
+  // トップへ戻るボタンからのスクロール
+  $('.pagetop').click(function(){
+  		$('html,body').animate({
+  				'scrollTop': 0
+  		}, 'slow');
+  });
+
+});
